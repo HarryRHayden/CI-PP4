f542f27928bc5ba677c180bd3cbea910ca7aa547
Harry Hayden <h_man1992@hotmail.com>
2022-10-17 13:44:44 +0000

    update debugging tag to receive from environ

--- sliceshare/settings.py
+++ sliceshare/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEBUGGING'
+DEBUG = os.environ.get('DEBUGGING')
 
 ALLOWED_HOSTS = ['sliceshare.herokuapp.com', 'localhost']
 

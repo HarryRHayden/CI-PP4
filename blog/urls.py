35dbfb61e2286b97db5a4d47463c681ab5d0638e
Harry Hayden <h_man1992@hotmail.com>
2022-10-19 10:08:26 +0000

    add index page and link url to home

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,11 @@
 from . import views
 from django.contrib.auth import views as auth_views
 from django.urls import path
+
+
+
+# URL's for the page
+
+urlpatterns = [
+    path("", views.PostList.as_view(), name="home"),
+]

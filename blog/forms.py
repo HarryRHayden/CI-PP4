1967fdff69721b646d468b4a7cdc8b504cc1f716
Harry Hayden <h_man1992@hotmail.com>
2022-10-20 14:44:31 +0000

    add styling to account pages

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Comment
+from django.contrib.auth.forms import UserChangeForm
+from django.conf import settings
+from django.contrib.auth.models import User
 
 
 class CommentForm(forms.ModelForm):
